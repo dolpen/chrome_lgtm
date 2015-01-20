96b7a39f83e178489fe07146c9756baf3efddcd2
dolpen <dolpen@dolpen.net>
2015-01-20 18:19:09 +0900

    init

---
+++ js/background.js
@@ -0,0 +1,20 @@
+/**
+ * Created by yamada on 2015/01/05.
+ */
+$(function () {
+    chrome.browserAction.onClicked.addListener(function (tab) {
+        $.getJSON("http://www.lgtm.in/g", function (e) {
+            var r = "[![LGTM](" + e.trackableImageUrl + ")](" + e.dataUrl + ")";
+            chrome.tabs.sendMessage(tab.id, {
+                    command: "lgtm_markdown",
+                    body: r
+                },
+                function (msg) {
+                    console.log(msg);
+                });
+        })();
+    });
+});
+
+
+
---
+++ js/content.js
@@ -0,0 +1,16 @@
+/**
+ * Created by yamada on 2015/01/05.
+ */
+$(function () {
+    chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+        if (!msg.command) return;
+        if (msg.command == 'lgtm_markdown') {
+            var $t = $("textarea[name='comment[body]'],textarea[name='content']");
+            var n = $t.val();
+            $t.val(
+                    (n + "\n\n" + msg.body).trim() + "\n"
+            );
+        }
+    });
+});
+
